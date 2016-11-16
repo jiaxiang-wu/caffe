8ba1c42c08b745e7c17f2cf0a1912b2988d4e4af
Jiaxiang Wu <jiaxiang.wu.90@gmail.com>
2016-11-16 09:58:12 +0800

    declare quan-inner-prod layer in the header and proto file

---
+++ include/caffe/layers/quan_inner_product_layer.hpp
@@ -0,0 +1,56 @@
+#ifndef CAFFE_QUAN_INNER_PRODUCT_LAYER_HPP_
+#define CAFFE_QUAN_INNER_PRODUCT_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief The parameter-quantized version of InnerProduct layer, also known as
+ *        a "fully-connected" layer, computes an inner product with a set of
+ *        learned sub-codebooks and quantization indicators, and (optionally)
+ *        adds biases.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class QuanInnerProductLayer : public Layer<Dtype> {
+ public:
+  explicit QuanInnerProductLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "QuanInnerProduct"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int dim_codeword_; // number of dimensions of each codeword
+  int cnt_codeword_; // number of codewords in each subspace
+  int M_;  // number of samples within the mini-batch
+  int K_;  // number of input dimensions
+  int N_;  // number of output dimensions
+  bool bias_term_;  // whether the bias term exists
+  Blob<Dtype> bias_multiplier_;  // use multiplication to add biases
+  bool transpose_;  // whether the weighting matrix is transposed
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_QUAN_INNER_PRODUCT_LAYER_HPP_
